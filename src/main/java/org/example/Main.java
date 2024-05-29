07b3009088f76bc71cf7ee4b1c936f8118337bc1
g21turbo <dirtdog556@gmail.com>
2024-05-28 22:16:58 -0700

    modified hybrid car and added objects main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car = new Car("2022 Toyota Corolla");
+        runRace(car);
+
+        Car ford = new GasPoweredCar("1968 Ford Mustang GT", 11.6, 8);
+        runRace(ford);
+
+        Car toyota = new GasPoweredCar("1986 Corolla GTS", 28.4, 4);
+        runRace(toyota);
+
+        Car tesla = new ElectricCar("2024 Red Tesla Model 3", 365, 75);
+        runRace(tesla);
+
+        Car prius = new HybridCar("2024 Toyota Prius", 350, 75, 4);
+        runRace(prius);
+
+
+
     }
+
+    public static void runRace(Car car) {
+        car.startEngine();
+        car.drive();
+    }
+
+
+
 }
